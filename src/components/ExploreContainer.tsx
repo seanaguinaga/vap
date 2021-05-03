d25455421f994f3fb731dcc994ba69b2bf8ee508
Sean Aguinaga <aguinaga.sean@gmail.com>
2021-05-03 13:16:20 -0700

    hmmmm

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,65 @@
-import './ExploreContainer.css';
+import { IonButton } from "@ionic/react";
+import React, { useLayoutEffect, useRef, useState } from "react";
+import ReactPlayer from "react-player";
+import { SizeMe } from "react-sizeme";
+import styled from "styled-components";
+//@ts-expect-error yes
+import video from "../resources/video.mp4";
+import "./ExploreContainer.css";
+import Stars from "./Stars";
+
+let Wrapper = styled.div`
+  position: relative;
+  padding-top: 56.25%; /* Player ratio: 100 / (1280 / 720) */
+`;
+
+let Player = styled(ReactPlayer)`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  let targetRef = useRef<any | null>(null);
+  let [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  useLayoutEffect(() => {
+    if (targetRef.current) {
+      setDimensions({
+        width: targetRef.current?.offsetWidth,
+        height: targetRef.current?.offsetHeight,
+      });
+    }
+  }, []);
+
   return (
-    <div className="container">
+    <>
+      <IonButton onClick={() => console.log({ dimensions })}>Click</IonButton>
       <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+      <Wrapper>
+        <SizeMe>
+          {({ size }) => (
+            <>
+              <div>My width is {size.width}px</div>
+
+              <Player
+                ref={targetRef}
+                url={video}
+                width="100%"
+                height="100%"
+                controls
+                playsinline
+              />
+            </>
+          )}
+        </SizeMe>
+      </Wrapper>
+      <Stars />
+    </>
   );
 };
 
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,11 +1,22 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
-import './Page.css';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonIcon,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { play } from "ionicons/icons";
+import { useParams } from "react-router";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Page.css";
 
 const Page: React.FC = () => {
-
-  const { name } = useParams<{ name: string; }>();
+  let { name } = useParams<{ name: string }>();
 
   return (
     <IonPage>
@@ -18,7 +29,7 @@ const Page: React.FC = () => {
         </IonToolbar>
       </IonHeader>
 
-      <IonContent fullscreen>
+      <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">{name}</IonTitle>
@@ -26,6 +37,15 @@ const Page: React.FC = () => {
         </IonHeader>
         <ExploreContainer name={name} />
       </IonContent>
+      <IonFooter>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonButton>
+              <IonIcon icon={play} />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonFooter>
     </IonPage>
   );
 };
