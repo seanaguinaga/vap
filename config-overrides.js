d99ffea73269d9a6280e4c29574e545ea8772099
Sean Aguinaga <aguinaga.sean@gmail.com>
2021-05-05 11:23:07 -0700

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,52 +1,17 @@
-const path = require("path");
-const { resolve } = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const { resolve } = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
-const devMode = process.env.NODE_ENV !== 'production'
+const wasmFile = resolve(__dirname, "node_modules", "mediainfo.js", "dist");
+const dist = resolve("build", "static", "js");
 
-const wasmFile = resolve(
-  __dirname,
-  'node_modules',
-  'mediainfo.js',
-  'dist',
-  'MediaInfoModule.wasm'
-);
-
-const dist = devMode ? '.' : resolve(__dirname);
-// const dist = resolve(__dirname);
-
-module.exports = function override(config, env) {
-  config.module.rules.push({
-    test: /\.worker\.js$/,
-    use: { loader: "workerize-loader" },
-  });
-
-  const wasmExtensionRegExp = /\.wasm$/;
-
-  config.resolve.extensions.push(".wasm");
-
-  config.module.rules.forEach((rule) => {
-    (rule.oneOf || []).forEach((oneOf) => {
-      if (oneOf.loader && oneOf.loader.indexOf("file-loader") >= 0) {
-        // Make file-loader ignore WASM files
-        oneOf.exclude.push(wasmExtensionRegExp);
-      }
-    });
-  });
-
-  // Add a dedicated loader for WASM
-  config.module.rules.push({
-    test: wasmExtensionRegExp,
-    include: path.resolve(__dirname, "src"),
-    use: [{ loader: require.resolve("wasm-loader"), options: {} }],
-  });
-
-  // template from https://www.npmjs.com/package/react-app-rewired
-  config.plugins.push(new CopyPlugin({
-    patterns: [
-      { from: wasmFile, to: dist },
-    ],
-  }),)
-      
-  return config;
+// template from https://www.npmjs.com/package/react-app-rewired
+module.exports = {
+  webpack: function (config, env) {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [{ from: wasmFile, to: dist }],
+      })
+    );
+    return config;
+  },
 };
