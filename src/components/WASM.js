24e3b7402e5e1b37515b45786701296999f65cd4
Sean Aguinaga <aguinaga.sean@gmail.com>
2021-05-05 11:22:37 -0700

    Update WASM.js

--- src/components/WASM.js
+++ src/components/WASM.js
@@ -1,14 +1,28 @@
-import worker from "../mediainfo.worker";
+import MediaInfo from "mediainfo.js";
 
-const { wasm } = worker();
+const readChunk = (file) => (chunkSize, offset) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      if (event.target.error) {
+        reject(event.target.error);
+      }
+      resolve(new Uint8Array(event.target.result));
+    };
+    reader.readAsArrayBuffer(file.slice(offset, offset + chunkSize));
+  });
 
 const handleChange = (event) => {
   const file = event.target.files[0];
-  wasm(file).then((result) => {
-    console.log(result);
+  MediaInfo().then((mediainfo) => {
+    mediainfo
+      .analyzeData(() => file.size, readChunk(file))
+      .then((result) => {
+        console.log(result);
+      });
   });
+  console.log(event.target.files[0]);
 };
-
 function WASM() {
   return <input type="file" onChange={handleChange} />;
 }
